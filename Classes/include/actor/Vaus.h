971a7ff84de7a240a079dc517c0c03f46e310b51
utkucnay <utkucnay@gmail.com>
2023-12-14 02:09:02 -0800

    feat(Vaus): Add new feats
    Add squeeze, limited move, and connect manager.

--- Classes/include/actor/Vaus.h
+++ Classes/include/actor/Vaus.h
@@ -1,27 +1,40 @@
 #ifndef __VAUS_H__
 #define __VAUS_H__
 
+#include "actor/IHit.h"
 #include "cocos2d.h"
+#include "component/AnimationComponent.h"
 #include "component/Component.h"
+#include "component/LimitedTargetMoveComponent.h"
+#include "component/SpriteSqueeze.h"
 #include "component/TargetMoveComponent.h"
 #include "diContainer/DIContainer.h"
+#include "manager/GameManager.h"
 #include "state/StateMachine.h"
 #include "component/InputHandleComponent.h"
 #include "component/HealthComponent.h"
 
+namespace Arkanoid::Manager {
+  class GameManager;
+}
+
 namespace Arkanoid {
-  class Vaus : public cocos2d::Node {
+  class Vaus : public cocos2d::Sprite, public IHit {
   public:
     bool init() override;
     CREATE_FUNC(Vaus);
+    ~Vaus() {
+    }
 
   public:
     void onEnter() override;
     void update(float delta) override;
-
+    void onExit() override;
   public:
     void inject(const DI::DIContainer& diContainer);
-
+    void hit(const cocos2d::Node& node) override;
+    void startDestroyVaus();
+    void endDestroyVaus();
   private:
     void onMove(const Components::InputHandle& inputHandle);
 
@@ -29,11 +42,14 @@ namespace Arkanoid {
     std::array<cocos2d::RefPtr<cocos2d::Sprite>, 6> _vausParts;
     std::array<cocos2d::RefPtr<cocos2d::Sprite>, 4> _vausAttackParts;
     std::unique_ptr<StateMachine> _stateMachine;
+    Manager::GameManager* _gameManager;
 
   private:
-    std::shared_ptr<Components::HealthComponent>        _healthComponent;
-    std::shared_ptr<Components::InputHandleComponent>   _inputHandleComponent;
-    std::shared_ptr<Components::TargetMoveComponent>    _targetMoveComponent;
+    std::shared_ptr<Components::HealthComponent>            _healthComponent;
+    std::shared_ptr<Components::InputHandleComponent>       _inputHandleComponent;
+    std::shared_ptr<Components::LimitedTargetMoveComponent> _targetMoveComponent;
+    std::shared_ptr<Components::AnimationComponent>         _destroyAnimComponent;
+    std::shared_ptr<Components::SpriteSqueeze>              _spriteSqueeze;
   };
 }
 
--- Classes/src/actor/Vaus.cpp
+++ Classes/src/actor/Vaus.cpp
@@ -1,6 +1,8 @@
 #include "actor/Vaus.h"
+#include "component/AnimationComponent.h"
 #include "component/HealthComponent.h"
-#include "diContainer/DIContainer.h"
+#include "component/LimitedTargetMoveComponent.h"
+#include "component/SpriteSqueeze.h"
 #include "resource/Resource.h"
 
 bool
@@ -34,10 +36,40 @@ void
 Arkanoid::Vaus::inject(
     const DI::DIContainer& diContainer)
 {
-  _targetMoveComponent = diContainer.getFactory<Components::TargetMoveComponent>();
-  _inputHandleComponent = diContainer.getFactory<Components::InputHandleComponent>();
-  _targetMoveComponent->setOwner(*this);
-  _inputHandleComponent->setOwner(*this);
+  if(diContainer.hasFactory<Components::LimitedTargetMoveComponent>()) {
+    _targetMoveComponent = diContainer.getFactory
+      <Components::LimitedTargetMoveComponent>();
+
+    _targetMoveComponent->setOwner(*this);
+  }
+  if(diContainer.hasFactory<Components::InputHandleComponent>()){
+    _inputHandleComponent = diContainer.getFactory
+      <Components::InputHandleComponent>();
+
+    _inputHandleComponent->setOwner(*this);
+  }
+  if(diContainer.hasFactory<Components::SpriteSqueeze>()){
+    _spriteSqueeze = diContainer.getFactory
+      <Components::SpriteSqueeze>();
+
+    _spriteSqueeze->setOwner(*this);
+  }
+  if(diContainer.hasFactory<Components::AnimationComponent>()){
+    _destroyAnimComponent = diContainer.getFactory
+      <Components::AnimationComponent>();
+
+    _destroyAnimComponent->setOwner(*this);
+    _destroyAnimComponent->setAnim(
+        *Resource::createVausExlodeAnim(),
+        *this);
+  }
+  if(diContainer.hasFactory<Components::HealthComponent>()) {
+    _healthComponent = diContainer.getFactory<Components::HealthComponent>();
+    _healthComponent->setOwner(*this);
+  }
+  if(diContainer.hasSingle<Manager::GameManager>())
+    _gameManager = diContainer.getSingle<Manager::GameManager>();
+
 }
 
 void
@@ -49,8 +81,18 @@ Arkanoid::Vaus::onEnter() {
   _inputHandleComponent->onMoved = CC_CALLBACK_1(
       Arkanoid::Vaus::onMove, this);
 
+  _gameManager->setVaus(this);
   Node::onEnter();
 
+  _targetMoveComponent->setLimit(
+      cocos2d::Vec2(32, 0),
+      cocos2d::Vec2(176, std::numeric_limits<float>::max()));
+
+  _destroyAnimComponent->subsEndAnim(
+      CC_CALLBACK_0(
+        Arkanoid::Vaus::endDestroyVaus, this)
+      );
+  _spriteSqueeze->setSqueeze(cocos2d::Vec2(1.25f, .75f));
   _targetMoveComponent->setMaxSpeed(500);
   _targetMoveComponent->onEnter();
   _inputHandleComponent->onEnter();
@@ -58,7 +100,12 @@ Arkanoid::Vaus::onEnter() {
 
 void
 Arkanoid::Vaus::update(float delta) {
-  _targetMoveComponent->update(delta);
+  _targetMoveComponent->tick(delta);
+  _spriteSqueeze->setScale(_targetMoveComponent->getSpeed() / 500);
+}
+
+void
+Arkanoid::Vaus::onExit() {
 }
 
 void
@@ -69,4 +116,23 @@ Arkanoid::Vaus::onMove(
   target.x = inputHandle.location.x;
   target.y = this->getPositionY();
   _targetMoveComponent->setTarget(target);
+}
+
+void
+Arkanoid::Vaus::hit(
+    const cocos2d::Node& node)
+{
+}
+
+void
+Arkanoid::Vaus::startDestroyVaus() {
+  if(_destroyAnimComponent->hasAnim())
+    _destroyAnimComponent->animate();
+  _targetMoveComponent->setEnabled(false);
+}
+
+void
+Arkanoid::Vaus::endDestroyVaus() {
+  _healthComponent->incrementHealth(-1);
+  _gameManager->endDestroyVaus(_healthComponent->isDeath());
 }
