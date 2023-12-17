7fcb03b56174870b09ff908acb8d2d8354b6412c
utkucnay <utkucnay@gmail.com>
2023-12-17 01:06:44 -0800

    refactor(asset): Change vaus asset
    Vaus assets are fit in procedural animations and change to static
    animations.

--- Classes/include/resource/VausResource.h
+++ Classes/include/resource/VausResource.h
@@ -7,38 +7,8 @@
 namespace Arkanoid::Resource {
   //Vaus Sprite
   inline static cocos2d::Sprite*
-  createVausLeftSprite() {
-    return createPixelSprite("Vaus/Vaus-Left.png");
-  }
-
-  inline static cocos2d::Sprite*
-  createVausMidSprite() {
-    return createPixelSprite("Vaus/Vaus-Mid.png");
-  }
-
-  inline static cocos2d::Sprite*
-  createVausRightSprite() {
-    return createPixelSprite("Vaus/Vaus-Right.png");
-  }
-
-  inline static cocos2d::Sprite*
-  createVausAttackLeftSprite() {
-    return createPixelSprite("Vaus/Vaus-Attack-Left.png");
-  }
-
-  inline static cocos2d::Sprite*
-  createVausAttackMidLeftSprite() {
-    return createPixelSprite("Vaus/Vaus-Attack-Mid-Left.png");
-  }
-
-  inline static cocos2d::Sprite*
-  createVausAttackMidRightSprite() {
-    return createPixelSprite("Vaus/Vaus-Attack-Mid-Right.png");
-  }
-
-  inline static cocos2d::Sprite*
-  createVausAttackRightSprite() {
-    return createPixelSprite("Vaus/Vaus-Attack-Right.png");
+  createVausSprite() {
+    return createPixelSprite("Vaus/Vaus.png");
   }
 
   inline static cocos2d::Animation*
